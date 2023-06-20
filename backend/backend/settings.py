a8b9a90771aa741a8a6b5055aa885bc420b281c5
timxt23 <112951950+timxt23@users.noreply.github.com>
2023-06-20 19:32:57 +0300

    fixed1

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -2,7 +2,7 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = 'foodgramm546939'
+SECRET_KEY = os.environ.get('SECRET_KEY', default='foodgramm546939')
 
 DEBUG = True
 
